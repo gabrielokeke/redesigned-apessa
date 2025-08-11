49f9459b5cd7426be09405b242e824af3f4585b2
gabrielokeke <okekegabriel84@gmail.com>
2025-08-11 12:55:32 +0100

    [IGNORED] eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Add your custom rules here as an override object:
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/jsx-key": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
